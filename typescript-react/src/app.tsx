e09af65ccafe3ec2cde87eecf94392b589ab9583
AyaanMahimwala <ayaanhaider@gmail.com>
2022-11-27 00:22:11 -0600

    console log craft data as markdown on button press

--- typescript-react/src/app.tsx
+++ typescript-react/src/app.tsx
@@ -1,6 +1,6 @@
-import * as React from "react"
-import * as ReactDOM from 'react-dom'
-import craftXIconSrc from "./craftx-icon.png"
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import craftXIconSrc from "./craftx-icon.png";
 
 const App: React.FC<{}> = () => {
   const isDarkMode = useCraftDarkMode();
@@ -13,36 +13,64 @@ const App: React.FC<{}> = () => {
     }
   }, [isDarkMode]);
 
-  return <div style={{
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  }}>
-    <img className="icon" src={craftXIconSrc} alt="CraftX logo" />
-    <button className={`btn ${isDarkMode ? "dark" : ""}`} onClick={insertHelloWorld}>
-      Hello world!
-    </button>
-  </div>;
-}
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <img className="icon" src={craftXIconSrc} alt="CraftX logo" />
+      <button
+        className={`btn ${isDarkMode ? "dark" : ""}`}
+        onClick={insertHelloWorld}
+      >
+        Print markdown in console
+      </button>
+    </div>
+  );
+};
 
 function useCraftDarkMode() {
   const [isDarkMode, setIsDarkMode] = React.useState(false);
 
   React.useEffect(() => {
-    craft.env.setListener(env => setIsDarkMode(env.colorScheme === "dark"));
+    craft.env.setListener((env) => setIsDarkMode(env.colorScheme === "dark"));
   }, []);
 
   return isDarkMode;
 }
 
-function insertHelloWorld() {
-  const block = craft.blockFactory.textBlock({
-    content: "Hello world!"
-  });
+async function insertHelloWorld() {
+  const result = await craft.dataApi.getCurrentPage();
 
-  craft.dataApi.addBlocks([block]);
+  if (result.status !== "success") {
+    throw new Error(result.message);
+  }
+
+  const pageBlock = result.data;
+  // Concatenate the text runs together to get the page title
+  const pageTitle = pageBlock.content.map((x) => x.text).join();
+  console.log(pageTitle);
+
+  const markdown = craft.markdown.craftBlockToMarkdown(
+    result.data.subblocks,
+    "common",
+    {
+      tableSupported: true,
+    }
+  );
+
+  console.log(markdown);
+
+  // const block = craft.blockFactory.textBlock({
+  //   content: "Hello world!",
+  // });
+
+  // craft.dataApi.addBlocks([block]);
 }
 
 export function initApp() {
-  ReactDOM.render(<App />, document.getElementById('react-root'))
+  ReactDOM.render(<App />, document.getElementById("react-root"));
 }
